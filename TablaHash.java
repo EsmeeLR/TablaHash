1abe1941e54bca81f0a94c9d744c6bc5213210cb
EsmeeLR <esmeralda.lunaromano14mail.com>
2025-11-23 18:00:59 -0600

    Se complementaron los métodos

--- TablaHash.java
+++ TablaHash.java
@@ -1,3 +1,29 @@
-public class TablaHash{
+import java.io.*;
+import java.util.*;
 
+public class TablaHash {
+
+    // Método para ordenar nombres
+    public static List<String> ordenarNombres(Set<String> nombres) {
+        List<String> lista = new ArrayList<>(nombres);
+        Collections.sort(lista); // orden alfabético
+        return lista;
+    }
+
+    // Método para imprimir lista de los nombres
+    public static void imprimirLista(List<String> lista) {
+        for (String nombre : lista) {
+            System.out.print(nombre + " ");
+        }
+        System.out.println();
+    }
+
+    // Guarda lista en archivo salida.txt
+    public static void guardarEnArchivo(List<String> lista, String nombreArchivo) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(nombreArchivo));
+        for (String nombre : lista) {
+            bw.write(nombre + " ");
+        }
+        bw.close();
+    }
 }
